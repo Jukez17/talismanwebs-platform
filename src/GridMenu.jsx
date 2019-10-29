2c6942cf6523dbee37d2eaad487dcfb655f707db
Jukka Koivu <skuuppi11@hotmail.com>
2019-10-29 17:15:50 +0200

    Grid menu
    Started grid menu for the platform menu

---
+++ src/GridMenu.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './GridMenu.css'
+
+function GridMenu() {
+    return (
+      <div className="Container">
+          <div className="Block">
+              <p className="Block-text">Test Block 1</p>
+          </div>
+          <div className="Block">
+              <p className="Block-text">Test Block 2</p>
+          </div>
+          <div className="Block">
+              <p className="Block-text">Test Block 3</p>
+          </div>
+          <div className="Block">
+              <p className="Block-text">Test Block 4</p>
+          </div>
+      </div>
+    );
+  }
+  
+  export default GridMenu;
+  
